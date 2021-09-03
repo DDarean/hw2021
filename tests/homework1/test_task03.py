0fbf7fff3c920d92b202363d762b14df2bbcc36b
Владислав Гоголин <ddarean@gmail.com>
2021-09-03 14:01:20 +0300

    task 4 added

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -1,11 +1,9 @@
 import os
+
 from homework1.task03 import find_maximum_and_minimum
 
 
 def test_one():
     filename = 'test_task03.txt'
     file_path = os.path.join(os.getcwd(), filename)
-    assert find_maximum_and_minimum(file_path), (-5, 7)
-
-
-print(test_one())
+    assert find_maximum_and_minimum(file_path) == (-5, 7)

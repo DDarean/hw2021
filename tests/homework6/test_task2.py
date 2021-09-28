6730d32b45081f8f6257ee0674213b0ae8949bcb
Владислав Гоголин <ddarean@gmail.com>
2021-09-28 13:18:44 +0300

    Homework 6. Task 2 fixed

--- tests/homework6/test_task2.py
+++ tests/homework6/test_task2.py
@@ -7,12 +7,12 @@ def test_get_created_instances_empty():
 
 def test_get_created_instances_three():
     user, _, _ = User(), User(), User()
-    assert User.get_created_instances() == 3
+    assert user.get_created_instances() == 3
 
 
 def test_reset_instances_return():
     user, _, _ = User(), User(), User()
-    assert User.get_created_instances() == user.reset_instances_counter()
+    assert user.get_created_instances() == user.reset_instances_counter()
 
 
 def test_reset_instances_result():

78ed73337ddc9dcd5a9971430cd0f159bbc58115
Владислав Гоголин <ddarean@gmail.com>
2021-10-26 19:42:59 +0300

    Homework 12. Imports fixed

--- homework12/create.py
+++ homework12/create.py
@@ -1,4 +1,4 @@
-from sqlalchemy import (Column, DateTime, ForeignKey, Integer, Interval,
+from sqlalchemy import (Column, DateTime, ForeignKey, Integer,
                         String, create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
